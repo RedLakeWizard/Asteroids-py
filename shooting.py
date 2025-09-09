136cc995f1ea9597af99e1990321d1f46d7bb5bc
RedLakeWizard <johnofred2@gmail.com>
2025-09-09 17:40:25 +0200

    Update shooting.py

--- shooting.py
+++ shooting.py
@@ -10,9 +10,7 @@ class Shot(CircleShape):
         self.velocity = pygame.Vector2(0, 1) * PLAYER_SHOOT_SPEED
 
     def draw(self, screen):
-        # sub-classes must override
          pygame.draw.circle(screen, "blue", self.position, self.radius, 2)
 
     def update(self, dt):
-        # sub-classes must override
        self.position += self.velocity * dt
